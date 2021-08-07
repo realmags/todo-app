234653f3cc3b89772f3a0e655fea7207e45b42c9
kent bryan maglinao <68115058+realmags@users.noreply.github.com>
2021-08-07 17:10:07 +0800

    Add working modal for creating tasks

---
+++ components/TaskForm.jsx
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Modal,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+} from "react-native";
+
+// ! taskInput update is not realtime, check taskInput.details
+export default function TaskForm({
+  toggleModal,
+  setToggleModal,
+  handleAddTask,
+}) {
+  const initialTaskInput = {
+    task: "",
+    details: "",
+  };
+  const [taskInput, setTaskInput] = useState(initialTaskInput);
+  const [focusInput, setFocusInput] = useState(false);
+
+  const handleToggleModal = () => {
+    setTaskInput(initialTaskInput);
+    Keyboard.dismiss();
+    setToggleModal(false);
+  };
+  const handleChangeText = (value, field) => {
+    let input = {};
+    input[field] = value;
+    setTaskInput((prevTaskInput) => ({ ...prevTaskInput, ...input }));
+  };
+
+  useEffect(() => {
+    if (!toggleModal) setFocusInput(false);
+    else
+      setTimeout(() => {
+        setFocusInput(true);
+      }, 100);
+  }, [toggleModal]);
+
+  return (
+    <Modal
+      visible={toggleModal}
+      animationType="fade"
+      onRequestClose={handleToggleModal}
+      transparent={true}
+    >
+      <View style={styles.modalWrapper}>
+        <TouchableWithoutFeedback onPress={handleToggleModal}>
+          <View style={styles.modalOverlay} />
+        </TouchableWithoutFeedback>
+        {/* write a task */}
+        {focusInput && (
+          <KeyboardAvoidingView style={styles.writeTaskWrapper}>
+            <TextInput
+              style={styles.primaryInput}
+              placeholder="New task"
+              onChangeText={(value) => handleChangeText(value, "task")}
+              autoFocus={true}
+            />
+            <TextInput
+              style={styles.secondaryInput}
+              placeholder="Add details"
+              onChangeText={(value) => handleChangeText(value, "details")}
+            />
+            {/* save task */}
+            <TouchableOpacity
+              style={styles.saveButtonWrapper}
+              onPress={() => handleAddTask(taskInput)}
+              disabled={taskInput.task === ""}
+            >
+              <Text
+                style={[
+                  styles.saveButton,
+                  { opacity: taskInput.task === "" ? 0.6 : 1 },
+                ]}
+              >
+                Save
+              </Text>
+            </TouchableOpacity>
+          </KeyboardAvoidingView>
+        )}
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  modalWrapper: {
+    flex: 1,
+  },
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: "#333333d5",
+  },
+  writeTaskWrapper: {
+    width: "100%",
+    position: "absolute",
+    bottom: 0,
+    paddingVertical: 20,
+    paddingHorizontal: 15,
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  primaryInput: {
+    fontSize: 16,
+  },
+  secondaryInput: {
+    fontSize: 14,
+  },
+  saveButtonWrapper: {
+    // backgroundColor: "#333333d5",
+    alignSelf: "flex-end",
+  },
+  saveButton: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#55bcf6",
+    paddingVertical: 5,
+  },
+});
---
+++ screens/home.jsx
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Keyboard,
+  FlatList,
+} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+// components
+import Task from "../components/Task";
+import TaskForm from "../components/TaskForm";
+
+// todo: edit weird shadow of button
+export default function Home() {
+  const [taskItems, setTaskItems] = useState([
+    { key: "1", task: "Buy dog food 🐶" },
+    { key: "2", task: "Feed cat 🐱" },
+    { key: "3", task: "Eat apple 🍎" },
+  ]);
+  const [toggleModal, setToggleModal] = useState(false);
+
+  const handleAddTask = (taskInput) => {
+    const newTaskItem = { ...taskInput, key: Math.random().toString() };
+    Keyboard.dismiss();
+    setToggleModal(false);
+    setTaskItems((prevTaskItems) => [...prevTaskItems, newTaskItem]);
+  };
+
+  return (
+    <View style={styles.container}>
+      {/* today's tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+        <View style={styles.items}>
+          <FlatList
+            data={taskItems}
+            renderItem={({ item }) => (
+              <TouchableOpacity>
+                <Task text={item.task} />
+              </TouchableOpacity>
+            )}
+          />
+        </View>
+      </View>
+      {/* write a task */}
+      <TaskForm
+        toggleModal={toggleModal}
+        setToggleModal={setToggleModal}
+        handleAddTask={handleAddTask}
+      />
+      <TouchableOpacity
+        style={styles.addWrapper}
+        onPress={() => setToggleModal(true)}
+      >
+        <View style={styles.addButton}>
+          <MaterialIcons name="add" size={26} color="#55bcf6" />
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#e8eaed",
+  },
+  tasksWrapper: {
+    flex: 1,
+    paddingTop: 80,
+    paddingHorizontal: 20,
+    paddingBottom: 80,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {
+    flex: 1,
+    marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 20,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#fff",
+    borderRadius: 60,
+    borderColor: "#c0c0c0",
+    borderWidth: 1,
+    width: 250,
+  },
+  addText: {
+    fontWeight: "bold",
+    fontSize: 24,
+  },
+  addWrapper: {
+    position: "absolute",
+    right: 20,
+    bottom: 30,
+  },
+  addButton: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#fff",
+    borderRadius: 60,
+    // ! weird shadow border
+    // elevation: 1,
+    shadowOffset: { width: 1, height: -2 },
+    shadowOpacity: 0.6,
+    shadowRadius: 60,
+    shadowColor: "#c0c0c0",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
